899be010ccd5ef0918af14567eda57823b151ee1
Vlad_Os <oshovskiyvlad28@gmail.com>
2025-09-19 02:18:34 +0300

    feat: implement sum, capitalize, reverseString, calculator, caesarCipher tests

---
+++ index.js
@@ -0,0 +1,67 @@
+export function sum(a, b) {
+  return a + b;
+}
+
+export function capitalize(string) {
+  return string[0].toUpperCase() + string.slice(1);
+}
+
+export function reverseString(string) {
+  let reversedString = '';
+  for (let i = 0; i < string.length; i++) {
+    reversedString += string.charAt(string.length - 1 - i);
+  }
+  return reversedString;
+}
+
+export const calculator = {
+  add(a, b) {
+    return a + b;
+  },
+  subtract(a, b) {
+    return a - b;
+  },
+  divide(a, b) {
+    return a / b;
+  },
+  multiply(a, b) {
+    return a * b;
+  },
+};
+
+export function caesarCipher(string, shift) {
+  let shiftedString = '';
+  let upperCaseBase = 65;
+  let lowerCaseBase = 97;
+  function isLetter(letter) {
+    return /[a-z]/i.test(letter);
+  }
+  function isUppercase(letter) {
+    return letter == letter.toUpperCase();
+  }
+  for (let i = 0; i < string.length; i++) {
+    let letterIndex;
+    if (isLetter(string[i])) {
+      if (isUppercase(string[i])) {
+        letterIndex = string.charCodeAt(i) - upperCaseBase;
+      } else {
+        letterIndex = string.charCodeAt(i) - lowerCaseBase;
+      }
+      let shiftedLetter = letterIndex + shift;
+      shiftedLetter = (shiftedLetter + 26) % 26;
+      if (isUppercase(string[i])) {
+        shiftedString += String.fromCharCode(shiftedLetter + upperCaseBase);
+      } else
+        shiftedString += String.fromCharCode(shiftedLetter + lowerCaseBase);
+    } else {
+      shiftedString += string[i];
+    }
+  }
+  return shiftedString;
+}
+
+export function analyzeArray(arr) {
+  return {
+    length: arr.length,
+  };
+}
